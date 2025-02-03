614d2342e2d903b2bb8f638e702a4b1cb43eb907
Florent Thomas <thomas.florent59970@protonmail.com>
2025-02-03 22:35:45 +0100

    Add inventory system and key retrieval mechanics; update styles and HTML structure

--- src/scenarios/scenario1/script.js
+++ src/scenarios/scenario1/script.js
@@ -5,16 +5,18 @@ createApp({
         return {
             userCode: '',
             exitCode: '',
-            correctAnswer: 'un écho',
-            newCode: '1234', // Le nouveau code à donner si l'énigme est résolue
-            coordinates: { x: 0, y: 0 }
+            correctAnswer: this.normalizeString('un écho'),
+            RiddleAnswer: '1',
+            coordinates: { x: 0, y: 0 },
+            hasKey: false
         };
     },
     methods: {
         submitCode() {
             // Vérification de la réponse de l'énigme
-            if (this.userCode.toLowerCase() === this.correctAnswer) {
-                alert(`Bonne réponse! Voici le nouveau code: ${this.newCode}`);
+            const normalizedUserCode = this.normalizeString(this.userCode);
+            if (normalizedUserCode === this.correctAnswer) {
+                alert(`Bonne réponse! Voici le nouveau nombre pour sortir: ${this.RiddleAnswer}`);
             } else {
                 alert('Mauvaise réponse. Veuillez réessayer.');
             }
@@ -22,7 +24,7 @@ createApp({
         submitExitCode() {
             // Logique pour soumettre le code de sortie
             if (this.exitCode === '3107') {
-                alert('Code correct! Vous pouvez sortir.');
+                alert('Code correct! Vous êtes sortie. Bravo!');
             } else {
                 alert('Code incorrect. Veuillez réessayer.');
             }
@@ -32,6 +34,21 @@ createApp({
             this.coordinates.y = event.clientY;
         },
 
+        getKey() {
+            this.hasKey = true; // Update the variable when the key is obtained
+            $('#modal2').modal('hide'); // Close the modal after getting the key
+        },
+        openArmoireModal(event) {
+            if (this.hasKey) {
+                $('#modal6').modal('show'); // Open the modal if the key is obtained
+            } else {
+                alert('Vous devez d\'abord récupérer la clé !'); // Show an alert if the key is not obtained
+            }
+        },
+
+        normalizeString(str) {
+            return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+        }
 
     },
     mounted() {
